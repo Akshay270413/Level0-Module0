8b5bc9f771b039a03037feb4e1401603e2da9009
league <bkartho@gmail.com>
2024-07-24 10:20:36 -0700

    I finish 2 parts of a module: monday I am 1 code away from finishing the module: tuesday I finished the module

--- _04_amazing_games/_b_high_low_game.py
+++ _04_amazing_games/_b_high_low_game.py
@@ -14,14 +14,20 @@ if __name__ == '__main__':
     # 3. Code a for loop to run steps 4-10, 10 times
     for i in range(10):
         # 4. Ask the user for a guess using a pop-up window, and save their response
-        name = simpledialog.askstring(title='greeter', message="pick a number between 1 and 100")
+        name = simpledialog.askinteger(title='greeter', prompt="pick a number between 1 and 100")
         # 5. If the guess is correct
-        if name=='60':
+        if name==random_num:
+            messagebox.showinfo(title='greeter', message="You Win")
+            sys.exit(0)
             # 6. Win. Use 'sys.exit(0)' to end the program
 
         # 7. if the guess is high
+        if name>random_num:
+            messagebox.showinfo(title='greeter', message="Too high, Try again")
             # 8. Tell them it's too high
         # 9. Else if the guess is low
+        elif name<random_num:
+            messagebox.showinfo(title='greeter', message="Too Low, Try again")
             # 10. Tell them it's too low
 
     #11. Outside of the loop, tell the user they lost
